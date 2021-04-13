e8428f18353ac6f78d837b11d6d54778386d00a8
MajidMpressive <bouikkenmajid@gmail.com>
2021-04-13 19:00:35 +0100

    Animated Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_bazaar/ui/components/AnimatedNavBar.dart';
+import 'package:my_bazaar/ui/components/AnimatedNavBarPage.dart';
 import 'package:my_bazaar/ui/pages/HomePage.dart';
 import 'package:my_bazaar/ui/pages/LoginPage.dart';
 import 'package:my_bazaar/ui/pages/ProductPage.dart';
@@ -16,7 +18,7 @@ class MyBazaarApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeChanger.lightTheme,
-      home: LoginPage(),
+      home: MainScreen(),
       routes: {
         "/mainScreen": (context) => MainScreen(),
       },
@@ -46,106 +48,36 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              // TabView
-              Container(
-                height: MediaQuery.of(context).size.height-MediaQuery.of(context).padding.top-44-16,
-                decoration: BoxDecoration(
-                    color: Theme.of(context).backgroundColor,
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(24))),
-                child: (() {
-                  switch (i) {
-                    case 0:
-                      return GestureDetector(
-                        onTap: (){
-                          setState(() {
-                            page=!page;
-                          });
-                        },
-                          child: page? ProductPage() : HomePage());
-                    case 1:
-                      return Container(
-                        color: Colors.white,
-                        child: Text('Two'),
-                      );
-                    case 2:
-                      return Container(
-                        color: Colors.white,
-                        child: Text('Three'),
-                      );
-                    case 3:
-                      return Container(
-                        color: Colors.white,
-                        child: Text('Four'),
-                      );
-                    default:
-                      return Container(
-                        color: Colors.white,
-                        child: Text('One'),
-                      );
-                  }
-                }()),
+        child: AnimatedNavBar(
+          color: Colors.white,
+          iconColor: Color(0xFF0D1C2E),
+          inactiveIconColor: Color(0xFF233B90),
+          backgroundColor: Colors.blue,
+          borderRadius: Radius.circular(24),
+          shadow: false,
+          textStyle: Theme.of(context).textTheme.headline4,
+          pages: [
+            AnimatedNavBarPage(
+              title: "home",
+              icon: Icons.home_filled,
+              pageContent: HomePage(),
+            ),
+            AnimatedNavBarPage(
+              title: "map",
+              icon: Icons.map,
+              inactiveIcon: Icons.map_outlined,
+              pageContent: ProductPage(),
+            ),
+            AnimatedNavBarPage(
+              title: "cart",
+              icon: Icons.shopping_cart,
+              pageContent: Container(
+                height: 80,
+                width: 80,
+                color: Colors.greenAccent,
               ),
-
-              // TabButtons
-              Container(
-                height: 44,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                        flex: (i == 0) ? 2 : 1,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            backgroundColor: (i == 0) ? Colors.white: ThemeColors.primaryColor,
-                          ),
-                            onPressed: () => setState(() => i = 0),
-                            child: Icon(
-                              MyBazaarIcons.home,
-                              color: (i == 0) ? ThemeColors.primaryColor: ThemeColors.backgroundColor,
-                            ))),
-                    Expanded(
-                        flex: (i == 1) ? 2 : 1,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: (i == 1) ? Colors.white: ThemeColors.primaryColor,
-                            ),
-                            onPressed: () => setState(() => i = 1),
-                            child: Icon(
-                              MyBazaarIcons.compass,
-                              color: (i == 1) ? ThemeColors.primaryColor: ThemeColors.backgroundColor,
-                            ))),
-                    Expanded(
-                        flex: (i == 2) ? 2 : 1,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: (i == 2) ? Colors.white: ThemeColors.primaryColor,
-                            ),
-                            onPressed: () => setState(() => i = 2),
-                            child: Icon(
-                              MyBazaarIcons.cart,
-                              color: (i == 2) ? ThemeColors.primaryColor: ThemeColors.backgroundColor,
-                            ))),
-                    Expanded(
-                        flex: (i == 3) ? 2 : 1,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: (i == 3) ? Colors.white: ThemeColors.primaryColor,
-                            ),
-                            onPressed: () => setState(() => i = 3),
-                            child: Icon(
-                              MyBazaarIcons.person,
-                              color: (i == 3) ? ThemeColors.primaryColor: ThemeColors.backgroundColor,
-                            ))),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
